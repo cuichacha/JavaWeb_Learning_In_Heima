e20c11a0b27af015fadf5b68538fb092ebf41144
Will Dufresne <chxdtc123@gmail.com>
2020-10-23 21:25:16 +0800

    Another commit of JavaWeb learning

---
+++ Java_Web_Core_Day02/src/Demo14/Demo14.java
@@ -0,0 +1,41 @@
+package Demo14;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Map;
+
+@WebServlet("/demo14")
+public class Demo14 extends HttpServlet {
+    private ArrayList<Student> list = new ArrayList<>();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+
+        Map<String, String[]> parameterMap = req.getParameterMap();
+
+        Class<Student> studentClass = Student.class;
+        try {
+            Student student = studentClass.newInstance();
+            for (String s : parameterMap.keySet()) {
+                Field declaredField = studentClass.getDeclaredField(s);
+                declaredField.setAccessible(true);
+                String[] values = parameterMap.get(s);
+                declaredField.set(student, values[0]);
+            }
+            System.out.println(student);
+            list.add(student);
+        } catch (InstantiationException | IllegalAccessException | NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+
+        resp.sendRedirect("/Java_Web_Core_Day02/SaveSuccess.html");
+
+    }
+}
