56b27dce7b1f8624d90274019cfa3fefc28fa2ae
Will Dufresne <chxdtc123@gmail.com>
2020-10-25 21:43:44 +0800

    Another commit of JavaWeb learning

---
+++ Java_Web_Core_Day03/src/AddStudents/AddStudents.java
@@ -0,0 +1,58 @@
+package AddStudents;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.function.Consumer;
+
+@WebServlet("/AddStudents")
+public class AddStudents extends HttpServlet {
+    private ArrayList<Student> list = new ArrayList<>();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=UTF-8");
+        int age = Integer.parseInt(req.getParameter("age"));
+        int score = Integer.parseInt(req.getParameter("score"));
+        if (age < 6 || age > 30 || score < 0 || score > 100) {
+            resp.getWriter().println("数据有误，2秒后跳转到首页，请重新输入");
+            resp.setHeader("refresh", "2;url=http://localhost:8080/Java_Web_Core_Day03/HomePage.html");
+        } else {
+            Map<String, String[]> parameterMap = req.getParameterMap();
+            Student student = new Student();
+
+            try {
+                BeanUtils.populate(student, parameterMap);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                e.printStackTrace();
+            }
+            list.add(student);
+            ServletContext servletContext = getServletContext();
+            String realPath = servletContext.getRealPath("/StuInfo.txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(realPath, true));
+            for (Student addStudent : list) {
+                try {
+                    bufferedWriter.write(addStudent.getName() + "," + addStudent.getAge() + "," + addStudent.getScore());
+                    bufferedWriter.newLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            bufferedWriter.close();
+            resp.getWriter().println("添加成功，2秒后自动跳转到首页");
+            resp.setHeader("refresh", "2;url=http://localhost:8080/Java_Web_Core_Day03/HomePage.html");
+        }
+    }
+}
---
+++ Java_Web_Core_Day03/src/AddStudents/ShowStudents.java
@@ -0,0 +1,29 @@
+package AddStudents;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+@WebServlet("/ShowStudents")
+public class ShowStudents extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=utf-8");
+        ServletContext servletContext = getServletContext();
+        String realPath = servletContext.getRealPath("/StuInfo.txt");
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(realPath));
+        String str = null;
+        while ((str = bufferedReader.readLine()) != null) {
+            resp.getWriter().println(str);
+            resp.getWriter().write("<br>");
+        }
+        bufferedReader.close();
+        resp.getWriter().write("<a href=\"/Java_Web_Core_Day03/HomePage.html\">回到首页</a>");
+    }
+}
