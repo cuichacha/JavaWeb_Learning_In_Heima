9aff5f349acd0c1e6baab2b47c1ec2b8f540e200
Will Dufresne <chxdtc123@gmail.com>
2020-11-19 14:24:14 +0800

    Another commit of JavaWeb learning

--- Redis/src/Code/List/RedisList.java
+++ Redis/src/Code/List/RedisList.java
@@ -6,11 +6,17 @@ import redis.clients.jedis.Jedis;
 public class RedisList {
     public static void main(String[] args) {
         Jedis jedis = JedisUtil.getJedis();
-        jedis.lpush("key2", "v1", "v2", "v3", "v4", "v5");
-        System.out.println(jedis.lindex("key2", 3));
-        System.out.println(jedis.llen("key2"));
-        System.out.println(jedis.lrange("key2", 0, -1));
-
+        jedis.lpush("key", "v2", "v3", "v4", "v5");
+        jedis.rpush("key", "v1");
+        System.out.println(jedis.lindex("key", 3));
+        System.out.println(jedis.llen("key"));
+        System.out.println(jedis.lrange("key", 0, -1));
+        System.out.println(jedis.lpop("key"));
+        System.out.println(jedis.rpop("key"));
+        jedis.rpush("key", "v1");
+        jedis.lpush("key", "v5", "v1", "v2", "v3", "v3", "v3");
+        jedis.lrem("key", 3, "v3");
+        jedis.brpop(10, "key");
         jedis.close();
     }
 }
--- Redis/src/Code/Set/RedisSet.java
+++ Redis/src/Code/Set/RedisSet.java
@@ -6,8 +6,9 @@ import redis.clients.jedis.Jedis;
 public class RedisSet {
     public static void main(String[] args) {
         Jedis jedis = JedisUtil.getJedis();
-
-
+        jedis.sadd("key", "v1", "v2", "v3", "v4", "v5", "v5");
+        System.out.println(jedis.smembers("key"));
+        System.out.println(jedis.scard("key"));
         jedis.close();
     }
 }
